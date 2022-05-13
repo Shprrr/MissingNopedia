dfaa8f85f29cd90b2504bbfab52c2553bc141d28
Maxime Talon <maximetalon@hotmail.com>
2022-05-13 13:05:27 -0400

    Get type, base stats and abilities from GraphQL

--- MissingNopedia/AdvancedSearch/AdvancedSearch.cs
+++ MissingNopedia/AdvancedSearch/AdvancedSearch.cs
@@ -20,6 +20,27 @@ namespace MissingNopedia.AdvancedSearch
     name: pokemon_v2_pokemonspeciesnames(where: {language_id: {_eq: 9}}) {
       name
     }
+    forms: pokemon_v2_pokemons(where: {is_default: {_eq: true}}) {
+      id
+      types: pokemon_v2_pokemontypes {
+        type: pokemon_v2_type {
+          name
+        }
+      }
+      baseStats: pokemon_v2_pokemonstats {
+        stat_id
+        base_stat
+      }
+      abilities: pokemon_v2_pokemonabilities {
+        slot
+        is_hidden
+        ability: pokemon_v2_ability {
+          name: pokemon_v2_abilitynames(where: {language_id: {_eq: 9}}) {
+            name
+          }
+        }
+      }
+    }
   }
 }", null, "PokeAPIquery");
 			var response = await client.SendQueryAsync(request, () => new { pokemons = new List<Pokemon>() });
--- MissingNopedia/AdvancedSearch/Pokemon.cs
+++ MissingNopedia/AdvancedSearch/Pokemon.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 
@@ -7,19 +9,40 @@ namespace MissingNopedia.AdvancedSearch
 {
 	public class Pokemon
 	{
+		[JsonProperty("forms")]
+		private readonly PokemonForm[] pokemonForms = new PokemonForm[1];
+		/// <summary>
+		/// Do not use this directly.
+		/// </summary>
+		private PokemonForm pokemonForm;
+		private string ability1 = "";
+		private string ability2 = "";
+		private string hiddenAbility = "";
+
+		/// <summary>
+		/// Get additional data for the current form.
+		/// </summary>
+		/// <returns></returns>
+		private PokemonForm GetPokemonForm()
+		{
+			if (pokemonForm == null)
+				pokemonForm = pokemonForms.Single(f => f.Id == Number);
+			return pokemonForm;
+		}
+
 		public int Number { get; set; }
 		[JsonConverter(typeof(PokemonNameConverter))]
 		public string Name { get; set; }
 		public string Form { get; set; }
-		public Type Type1 { get; set; }
-		public Type? Type2 { get; set; }
+		public Type Type1 => GetPokemonForm().Types[0];
+		public Type? Type2 => GetPokemonForm().Types.Length > 1 ? GetPokemonForm().Types[1] : null;
 
-		public int BaseHP { get; set; }
-		public int BaseAttack { get; set; }
-		public int BaseDefense { get; set; }
-		public int BaseSpAttack { get; set; }
-		public int BaseSpDefense { get; set; }
-		public int BaseSpeed { get; set; }
+		public int BaseHP => GetPokemonForm().BaseStats[0].BaseStat;
+		public int BaseAttack => GetPokemonForm().BaseStats[1].BaseStat;
+		public int BaseDefense => GetPokemonForm().BaseStats[2].BaseStat;
+		public int BaseSpAttack => GetPokemonForm().BaseStats[3].BaseStat;
+		public int BaseSpDefense => GetPokemonForm().BaseStats[4].BaseStat;
+		public int BaseSpeed => GetPokemonForm().BaseStats[5].BaseStat;
 
 		public int TotalStat { get { return BaseHP + BaseAttack + BaseDefense + BaseSpAttack + BaseSpDefense + BaseSpeed; } }
 		public int PhysicalSweeper { get { return BaseAttack + BaseSpeed; } }
@@ -28,9 +51,33 @@ namespace MissingNopedia.AdvancedSearch
 		public int PhysicalTank { get { return BaseAttack + BaseDefense; } }
 		public int SpecialTank { get { return BaseSpAttack + BaseSpDefense; } }
 
-		public string Ability1 { get; set; }
-		public string Ability2 { get; set; }
-		public string HiddenAbility { get; set; }
+		public string Ability1
+		{
+			get
+			{
+				if (ability1 == "")
+					ability1 = GetPokemonForm().Abilities.Single(a => a.Slot == 1).Ability.Name;
+				return ability1;
+			}
+		}
+		public string Ability2
+		{
+			get
+			{
+				if (ability2 == "")
+					ability2 = GetPokemonForm().Abilities.SingleOrDefault(a => a.Slot == 2)?.Ability.Name;
+				return ability2;
+			}
+		}
+		public string HiddenAbility
+		{
+			get
+			{
+				if (hiddenAbility == "")
+					hiddenAbility = GetPokemonForm().Abilities.SingleOrDefault(a => a.IsHidden)?.Ability.Name;
+				return hiddenAbility;
+			}
+		}
 
 		public Pokemon(int number, string name)
 		{
@@ -60,7 +107,7 @@ namespace MissingNopedia.AdvancedSearch
 			row.Cells.Add(new DataGridViewTextBoxCell
 			{
 				Value = Type2,
-				Style = new DataGridViewCellStyle { BackColor = Type2.HasValue ? Type2.Value.BackColor() : Color.White }
+				Style = new DataGridViewCellStyle { BackColor = Type2.HasValue ? Type2.Value.BackColor() : Color.Empty }
 			});
 
 			row.Cells.Add(new DataGridViewTextBoxCell { Value = BaseHP });
@@ -82,6 +129,37 @@ namespace MissingNopedia.AdvancedSearch
 			return row;
 		}
 
+		private class PokemonForm
+		{
+			public int Id { get; set; }
+			[JsonConverter(typeof(PokemonTypesConverter))]
+			public Type[] Types { get; set; }
+			public PokemonStat[] BaseStats { get; set; }
+			public PokemonAbility[] Abilities { get; set; }
+
+			public class PokemonStat
+			{
+				[JsonProperty("stat_id")]
+				public int StatId { get; set; }
+				[JsonProperty("base_stat")]
+				public int BaseStat { get; set; }
+			}
+
+			public class PokemonAbility
+			{
+				public int Slot { get; set; }
+				[JsonProperty("is_hidden")]
+				public bool IsHidden { get; set; }
+				public PokemonAbilityAbility Ability { get; set; }
+
+				public class PokemonAbilityAbility
+				{
+					[JsonConverter(typeof(PokemonNameConverter))]
+					public string Name { get; set; }
+				}
+			}
+		}
+
 		private class PokemonNameConverter : JsonConverter<string>
 		{
 			private class PokemonName
@@ -93,7 +171,27 @@ namespace MissingNopedia.AdvancedSearch
 			{
 				return serializer.Deserialize<PokemonName[]>(reader)[0].Name;
 			}
-			public override void WriteJson(JsonWriter writer, [AllowNull] string value, JsonSerializer serializer) => throw new System.NotImplementedException();
+
+			public override void WriteJson(JsonWriter writer, [AllowNull] string value, JsonSerializer serializer) => throw new NotImplementedException();
+		}
+
+		private class PokemonTypesConverter : JsonConverter<Type[]>
+		{
+			private class PokemonTypes
+			{
+				public class PokemonType
+				{
+					public string Name { get; set; }
+				}
+				public PokemonType Type { get; set; }
+			}
+
+			public override Type[] ReadJson(JsonReader reader, System.Type objectType, [AllowNull] Type[] existingValue, bool hasExistingValue, JsonSerializer serializer)
+			{
+				return serializer.Deserialize<PokemonTypes[]>(reader).Select(t => new Type(Enum.Parse<Type.TypeValue>(t.Type.Name, true))).ToArray();
+			}
+
+			public override void WriteJson(JsonWriter writer, [AllowNull] Type[] value, JsonSerializer serializer) => throw new NotImplementedException();
 		}
 	}
 }
